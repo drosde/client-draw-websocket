b7b781fa8c9b1e56f97913070ff66fec693638ca
drosde <enagu5@hotmail.com>
2018-11-03 04:07:12 -0300

    chat improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,9 +9,11 @@ import { PlayComponent } from './pages/play/play.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 /** PrimeNG */
-import {ButtonModule} from 'primeng/button';
-import {InputTextareaModule} from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 import { FormsModule } from '@angular/forms'; // required for textarea - inputs with bindings
+import { ScrollPanelModule} from 'primeng/scrollpanel';
+
 
 import { WebSocketService } from './services/web-socket.service';
 import { ChatService } from './services/chat.service';
@@ -48,8 +50,10 @@ const appRoutes: Routes = [
     WebSocketService,
     ChatService,
     DrawSocketService,
-    DrawHelperService
+    DrawHelperService,
+    ScrollPanelModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/pages/play/play.component.ts
+++ src/app/pages/play/play.component.ts
@@ -1,42 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ChatComment } from '../../models/chatcomment';
 import { ChatService } from '../../services/chat.service';
 import { DrawSocketService } from '../../services/draw-socket.service';
 import { DrawHelperService } from '../../services/draw-helper.service';
-
 
 @Component({
   selector: 'app-play',
   templateUrl: './play.component.html',
   styleUrls: ['./play.component.scss']
 })
+
 export class PlayComponent implements OnInit {
+  @ViewChild('chatScroll') private chatScroll: ElementRef;
 
   chatComments: Array<ChatComment>;
   comment:string;
-  
+  chatElem:HTMLElement;
+
   constructor(
     private chatservice:ChatService, private drawservice:DrawSocketService,
     private drawHelper:DrawHelperService
     ) { }
 
   ngOnInit() {
-    this.chatComments = [
-      {author: "carlitox", content: "hola"},
-      {author: "cax", content: "hello"}
-    ];
+    this.chatElem = document.querySelector('.chat');
+    this.chatComments = [];
     
     this.chatservice.getComms().subscribe(comm => {
       console.log("Comentario recibido", comm);
       this.chatComments.push(comm);
+
+      // this.chatElem.scrollTop = this.chatElem.scrollHeight * 200 - this.chatElem.clientHeight;
     });
 
     let canvas = document.getElementById('main-canvas') as HTMLCanvasElement;
     this.drawHelper.setUp(canvas);
   }
 
-  sendComment(msg:string) {
-    if(this.comment && this.comment.length > 0){
+  sendComment(msg:string, event?:any) {
+    // prevent default event
+    if(event) event.preventDefault();
+
+    msg = msg.trim();
+
+    if(msg && msg.length > 0){
       let comm:ChatComment = {
         author: this.chatservice.username,
         content: msg
